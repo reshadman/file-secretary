fc5fe75803084b35fb3c1bf9177352964dd8b501
Reza Shadman <pcfeeler@gmail.com>
2017-08-30 14:45:14 +0430

    Add helper function for asset

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,8 @@
 <?php
+
+if ( ! function_exists('fs_asset')) {
+    function fs_asset($assetFolder, $afterPath, $force = false)
+    {
+        return \Reshadman\FileSecretary\Infrastructure\UrlGenerator::asset($assetFolder, $afterPath, $force);
+    }
+}
