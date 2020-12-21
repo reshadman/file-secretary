d00089e1dbb1774e52a1d39e18afa5aec5a447fe
Reza <reza@hey.com>
2020-12-21 16:35:18 +0330

    Add rar mime type support

--- src/Infrastructure/MimeDbRepository.php
+++ src/Infrastructure/MimeDbRepository.php
@@ -6,9 +6,20 @@ use MimeTyper\Repository\MimeDbRepository as BaseMimeDbRepository;
 
 class MimeDbRepository extends BaseMimeDbRepository
 {
+    protected $extendedMimes = [
+        'application/x-rar-compressed' => 'rar',
+        'application/rar' => 'rar'
+    ];
+
     public function findExtension($type)
     {
         $found = parent::findExtension($type);
+
+        if ($found === null) {
+            if (isset($this->extendedMimes[$type])) {
+                $found = $this->extendedMimes[$type];
+            }
+        }
 
         return FileSecretaryManager::normalizeExtension($found);
     }
